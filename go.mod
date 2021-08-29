8cc1d9175ee64ccc26706373815c061a89be3052
Justin Casso <justincasso1@gmail.com>
2021-08-29 05:17:34 -0700

    Temporary for transfer

--- go.mod
+++ go.mod
@@ -1,3 +1,19 @@
 module github.com/jrcasso/tugboat
 
+replace github.com/jrcasso/tugboat => /workspaces/tugboat/
+
 go 1.17
+
+require (
+	github.com/google/go-github v17.0.0+incompatible
+	github.com/sirupsen/logrus v1.8.1
+	golang.org/x/oauth2 v0.0.0-20180821212333-d2e6202438be
+)
+
+require (
+	github.com/golang/protobuf v1.3.2 // indirect
+	github.com/google/go-querystring v1.0.0 // indirect
+	golang.org/x/net v0.0.0-20190311183353-d8887717615a // indirect
+	golang.org/x/sys v0.0.0-20191026070338-33540a1f6037 // indirect
+	google.golang.org/appengine v1.1.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,4 +1,52 @@
 package main
 
+import (
+	"context"
+	"os"
+
+	"github.com/google/go-github/github"
+	log "github.com/sirupsen/logrus"
+	"golang.org/x/oauth2"
+)
+
+var requiredEnvs = [...]string{
+	"GITHUB_TOKEN",
+	"GITHUB_ORGANIZATION",
+}
+
 func main() {
+	initializeLogging()
+	validateEnvironment()
+
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: os.Getenv("GITHUB_TOKEN")})
+	tc := oauth2.NewClient(ctx, ts)
+
+	client := github.NewClient(tc)
+
+	repo := &github.Repository{
+		Name:        github.String("test-repo"),
+		Private:     github.Bool(true),
+		Description: github.String("Test repository"),
+	}
+	repo, _, err := client.Repositories.Create(ctx, os.Getenv("GITHUB_ORGANIZATION"), repo)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Infof("Successfully created new repo: %v\n", repo.GetName())
+}
+
+func validateEnvironment() {
+	for i := 0; i < len(requiredEnvs); i++ {
+		var env = os.Getenv(requiredEnvs[i])
+		if env == "" {
+			log.Fatalf("Required environment variable not set: %v", requiredEnvs[i])
+		}
+	}
+}
+
+func initializeLogging() {
+	// TODO: Implement dynamic log level, output, and format switches
+	log.SetLevel(log.DebugLevel)
+	log.SetOutput(os.Stdout)
 }
